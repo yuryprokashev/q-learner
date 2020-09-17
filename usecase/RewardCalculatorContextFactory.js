6e543596e602602d1435ee6c80f0083788d3cb98
yuryprokashev <yury.prokashev@edufun.me>
2020-09-17 09:00:22 +0300

    building timebuckets with environment objects

--- usecase/RewardCalculatorContextFactory.js
+++ usecase/RewardCalculatorContextFactory.js
@@ -7,14 +7,39 @@ module.exports = RewardCalculatorContextFactory;
  *
  * @constructor
  */
-function RewardCalculatorContextFactory(environmentRepository, parameterRepository){
+function RewardCalculatorContextFactory(repositoryService, factoryService){
+    const DEFAULT_STEP = 1000;
+    const _environmentRepository = repositoryService.getEnvironmentRepository();
+    const _timeBucketFactory = factoryService.getTimeBucketFactory();
     this.create = useCaseRequest =>{
-        let query = parameterRepository.createQueryBuilder()
-            .setCreatedFrom(useCaseRequest.getStart())
-            .setCreatedUntil(useCaseRequest.getEnd())
+        let createdFromCriterion = new CriterionBuilder()
+            .setFieldName("createdAt")
+            .gte()
+            .setExpectedValue(useCaseRequest.start)
             .build();
-        let parameters = parameterRepository.getByQuery(query);
-        return new RewardCalculatorContext(parameters);
+        let createdUntilCriterion = new CriterionBuilder()
+            .setExpectedValue(useCaseRequest.end)
+            .lte()
+            .setFieldName("createdAt")
+            .build();
+        let query = new CriteriaBuilder(createdFromCriterion).and(createdUntilCriterion).build();
+        let environments = _environmentRepository.getByQuery(query);
+        let firstEnvironment = environments[0];
+        let lastEnvironment = environments[environments.length - 1];
+        let timebuckets = _timeBucketFactory.fromConfig({
+            start: firstEnvironment.getCreatedAt(),
+            end: lastEnvironment.getCreatedAt(),
+            length: useCaseRequest.period,
+            step: useCaseRequest.step || DEFAULT_STEP
+        });
+
+        timebuckets.forEach(bucket =>{
+            environments.forEach(env =>{
+                if(bucket.includes(env.getCreatedAt())) bucket.add(env);
+            });
+        });
+
+        return new RewardCalculatorContext(environments, timebuckets);
     };
 }
 
@@ -22,8 +47,11 @@ function RewardCalculatorContextFactory(environmentRepository, parameterReposito
  *
  * @constructor
  */
-function RewardCalculatorContext(parameters){
-    this.getParameters = () =>{
-        return parameters;
+function RewardCalculatorContext(environments, timebuckets){
+    this.getBuckets = () =>{
+        return timebuckets;
+    };
+    this.getEnvironments = ()=>{
+        return environments;
     };
 }
