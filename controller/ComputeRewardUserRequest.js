1172e1cbc0b1132af4c57ed7498c73cdd2f8bee9
yuryprokashev <yury.prokashev@edufun.me>
2020-09-17 08:58:31 +0300

    adding step to user request

--- controller/ComputeRewardUserRequest.js
+++ controller/ComputeRewardUserRequest.js
@@ -3,15 +3,17 @@ module.exports = ComputeRewardsRequest;
 /**
  *
  * @param {string} startDay - the start day of the reward calculation period (UTC)
- * @param {number} dayCount - the duration of the reward calculation period
+ * @param {number} dayCount - the duration of the reward calculation period (minutes)
  * @param {number} rewardPeriod - the size of the time window where reward is computed
  * @param {string} rewardType - either "buy" or "sell"
+ * @param {number }step - the step between reward calculation periods (minutes)
  * @constructor
  */
-function ComputeRewardsRequest(startDay, dayCount, rewardPeriod, rewardType){
+function ComputeRewardsRequest(startDay, dayCount, rewardPeriod, rewardType, step){
     this.start = startDay;
     this.dayCount = dayCount;
     this.rewardPeriod = rewardPeriod;
     this.rewardType = rewardType;
     this.debug = true;
+    this.step = step;
 }
