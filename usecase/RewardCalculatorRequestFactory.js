9e257abd83123031c580f23eccd01460e0c5c987
yuryprokashev <yury.prokashev@edufun.me>
2020-09-17 08:59:40 +0300

    using step from user request in reward calculator request. fix: start date and end date computed wrong

--- usecase/RewardCalculatorRequestFactory.js
+++ usecase/RewardCalculatorRequestFactory.js
@@ -13,11 +13,12 @@ function RewardCalculatorRequestFactory(){
      */
     this.create = userRequest =>{
         let userStart = userRequest.start.split("-").map(str => {return parseInt(str);});
-        let startUtc = Date.UTC(userStart[0], userStart[1], userStart[2]);
-        let endUtc = startUtc + userRequest.dayCount * DAY_MS;
+        let startUtc = Date.UTC(userStart[0], userStart[1] - 1, userStart[2]);
+        let endUtc = startUtc + (userRequest.dayCount + 1) * DAY_MS;
         let period = userRequest.rewardPeriod * MIN_MS;
         let isBuy = userRequest.rewardType.toLowerCase() === "buy";
-        return new RewardCalculatorRequest(startUtc, endUtc, period, isBuy);
+        let step = userRequest.step ? userRequest.step * MIN_MS : undefined;
+        return new RewardCalculatorRequest(startUtc, endUtc, period, isBuy, step);
     };
 }
 
@@ -26,19 +27,13 @@ function RewardCalculatorRequestFactory(){
  * @param {number} end - the epoch timestamp of end time (ms)
  * @param {number} period - the duration of the reward period (ms)
  * @param {boolean} isBuy - buy or not buy (sell)
+ * @param {number} [step=10000] - step
  * @constructor
  */
-function RewardCalculatorRequest(start, end, period, isBuy){
-    this.getStart = ()=>{
-        return start;
-    };
-    this.getEnd = ()=>{
-        return end;
-    };
-    this.getPeriod = ()=>{
-        return period;
-    };
-    this.isBuy = ()=>{
-        return isBuy;
-    };
+function RewardCalculatorRequest(start, end, period, isBuy, step){
+    this.start = start;
+    this.end = end;
+    this.period = period;
+    this.isBuy = isBuy;
+    this.step = step || 10000;
 }
