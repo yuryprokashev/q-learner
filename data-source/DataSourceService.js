08c4a79fcffb5fbf01bb62b0e28919e52bf5a2a2
yuryprokashev <yury.prokashev@edufun.me>
2020-09-19 14:08:37 +0300

    extend datasource service with file datasource

--- data-source/DataSourceService.js
+++ data-source/DataSourceService.js
@@ -1,14 +1,19 @@
 const Database = require("better-sqlite3");
+const fs = require("fs");
+const File = require("../model/File");
 module.exports = DataSourceService;
 function DataSourceService(){
     let _db;
-    this.getDatabaseConnection = (fileName, verbose) =>{
+    this.getDatabaseConnection = (filePath, verbose) =>{
         if(!_db) {
             let options = {};
             if(verbose) options.verbose = verbose;
-            _db = new Database(fileName, options);
+            _db = new Database(filePath, options);
             process.on('exit', () => _db.close());
         }
         return _db;
+    };
+    this.getFile = filePath =>{
+        return new File(filePath, fs.readFileSync(filePath, "utf8"));
     };
 }
