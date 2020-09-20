cb61916dc0bddecd21fc2666cf2fc8e0a9474901
yuryprokashev <yury.prokashev@edufun.me>
2020-09-20 13:04:41 +0300

    add getters for data to be persisted

--- model/VitrualOrder.js
+++ model/VitrualOrder.js
@@ -1,11 +1,35 @@
 module.exports = VirtualOrder;
-function VirtualOrder(timeslot, environments){
+
+/**
+ * Виртуальный ордер помогает рассчитать награду для стратегий Продавец и Покупатель.
+ * Виртуальный ордер имеет точку отправки и точку исполнения. В реальном мире это две разные точки.
+ * Каждая такая точка - это объект типа Environment.
+ * @param id
+ * @param timeslot
+ * @param environments
+ * @param executionDelay
+ * @constructor
+ */
+function VirtualOrder(id, timeslot, environments, executionDelay){
     let _reward;
+    let _executionDelay = executionDelay || 0;
+    this.getId = ()=>{
+        return id;
+    };
+    this.getExecutionDelay = ()=>{
+        return _executionDelay;
+    };
+    this.getOrderSentEnvironment = ()=>{
+        return _getOrderSentEnvironment();
+    };
+    this.getOrderExecutedEnvironment = ()=>{
+        return _getOrderExecutedEnvironment();
+    };
     this.getReward = type =>{
-        if(!_reward) _init();
+        if(!_reward) _computeReward();
         return _reward[type];
     };
-    function _init(){
+    function _computeReward(){
         _reward = {buy: 0, sell: 0};
         /*
         Bid is always less than Ask.
@@ -26,9 +50,9 @@ function VirtualOrder(timeslot, environments){
         Now when computing the Buy reward, our entry price is the ask of the first environment.
         Whilst, when computing the Sell reward, our entry price is the bid of the first environment.
          */
-        let firstE = environments[0];
-        let entryBuy = firstE.getParameter("ask").getValue();
-        let entrySell = firstE.getParameter("bid").getValue();
+        let environmentAtOrderExecution = _getOrderExecutedEnvironment();
+        let entryBuy = environmentAtOrderExecution.getParameter("ask").getValue();
+        let entrySell = environmentAtOrderExecution.getParameter("bid").getValue();
 
         /*
         Finally setting the reward.
@@ -37,5 +61,18 @@ function VirtualOrder(timeslot, environments){
          */
         _reward.buy = maxBid - entryBuy;
         _reward.sell = entrySell - minAsk;
+    }
+    function _getOrderExecutedEnvironment(){
+        let currentEnvironment;
+        for(let i = 0; i < environments.length; i++){
+            currentEnvironment = environments[i];
+            if(currentEnvironment.getCreatedAt() >= _getOrderSentEnvironment().getCreatedAt() + _executionDelay) {
+                break;
+            }
+        }
+        return currentEnvironment;
+    }
+    function _getOrderSentEnvironment(){
+        return environments[0];
     }
 }
