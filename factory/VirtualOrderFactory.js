895f1585bc9485921f7eed7c2e955d638b7a3c86
yuryprokashev <yury.prokashev@edufun.me>
2020-09-20 13:03:37 +0300

    function to produce virtual order id

--- factory/VirtualOrderFactory.js
+++ factory/VirtualOrderFactory.js
@@ -2,7 +2,16 @@ const VirtualOrder = require("../model/VitrualOrder");
 module.exports = VirtualOrderFactory;
 
 function VirtualOrderFactory(){
-    this.fromTimeBucket = timeBucket =>{
-        return new VirtualOrder(timeBucket.getTimeslot(), timeBucket.getObjects());
+    this.fromTimeBucket = (timeBucket, executionDelay) =>{
+        let delay = executionDelay || 0;
+        let firstEnv = timeBucket.getObjects()[0];
+        let symbol = firstEnv.getSymbol();
+        let sent = firstEnv.getCreatedAt();
+        let duration = timeBucket.getTimeslot().getDuration();
+        let id = _virtualOrderId(symbol, sent, duration,delay);
+        return new VirtualOrder(id, timeBucket.getTimeslot(), timeBucket.getObjects(), delay);
     };
+    function _virtualOrderId(symbol, sent, duration, executionDelay){
+        return `v-order-${symbol}-${sent}-${duration}-${executionDelay}`;
+    }
 }
