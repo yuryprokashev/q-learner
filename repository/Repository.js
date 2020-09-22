46785196aaa1b174031b15bba44271c6b632bbf1
yuryprokashev <yury.prokashev@edufun.me>
2020-09-22 20:24:42 +0300

    repositories now are not aware of sql definitions and column mappings. They use gateways instead.

--- repository/Repository.js
+++ repository/Repository.js
@@ -1,52 +1,51 @@
-const SqlTableGateway = require("./SqlTableGateway");
-module.exports.SqlBuilder = SqlRepositoryBuilder;
+const Validator = require("../basic/Validator");
+module.exports.Builder = RepositoryBuilder;
 
 /**
  *
  * @constructor
  */
-function SqlRepositoryBuilder(){
-    let _db;
-    let _sqlDefinition, _entityFactory;
-    let _mappings = [];
-    this.setDbConnection = db =>{
-        _db = db;
+function RepositoryBuilder(){
+    let _entityFactory, _gatewayRequestFactory, _reader, _writer;
+    this.setReader = reader =>{
+        _reader = reader;
         return this;
     };
-    this.setDefinition = str =>{
-        _sqlDefinition = str;
+    this.setWriter = writer =>{
+        _writer = writer;
         return this;
     };
-    this.addMapping = (strFrom, strTo)=>{
-        _mappings.push([strFrom, strTo]);
-        return this;
-    };
-    this.setMappings = mappings =>{
-        _mappings = mappings;
-        return this;
-    };
-    this.setFactory = factory =>{
+    this.setEntityFactory = factory =>{
         _entityFactory = factory;
         return this;
     };
+    this.setGatewayRequestFactory = factory =>{
+        _gatewayRequestFactory = factory;
+        return this;
+    };
     this.build = ()=>{
-        return new SqlRepository(_db, _sqlDefinition, _mappings, _entityFactory);
+        return new Repository(_reader,  _writer, _gatewayRequestFactory, _entityFactory);
     };
 }
 
 /**
  *
- * @param db
- * @param definition
- * @param mappings
- * @param factory
+ * @param reader
+ * @param writer
+ * @param gatewayRequestFactory
+ * @param entityFactory
  * @constructor
  */
-function SqlRepository(db, definition, mappings, factory){
-    const _tableGateway = new SqlTableGateway(db, definition);
+function Repository(reader, writer, gatewayRequestFactory, entityFactory){
     this.getByQuery = criteria =>{
-        let sql = criteria.asSql(mappings);
-        let records = _tableGateway.read(sql);
-        return factory.fromRecords(records);
+        Validator.isDefined("Table Reader", reader, "Read operation is not supported.");
+        let where = gatewayRequestFactory.where(criteria);
+        let records = reader.read(where);
+        return entityFactory.fromRecords(records);
+    };
+    this.insert = criteria =>{
+        Validator.isDefined("Table Writer", writer, "Insert operation is not supported.");
+        let insert = gatewayRequestFactory.insert(criteria);
+        writer.write(insert);
     };
 }
--- repository/RepositoryService.js
+++ repository/RepositoryService.js
@@ -1,24 +1,23 @@
-const SqlRepositoryBuilder = require("./Repository").SqlBuilder;
+const RepositoryBuilder = require("./Repository").Builder;
 module.exports = RepositoryService;
 
-function RepositoryService(dataSourceService, factoryService){
-    const DB_FILE = "C:/Users/yuryp/AppData/Roaming/MetaQuotes/Terminal/Common/Files/mt5.sqlite";
-    const ENVIRONMENT_SQL = "select p.id, e.id as parent_id, e.symbol, e.created, p.name, p.value from parameters as p left join environments as e on e.id = p.parent_id";
-    const ENVIRONMENT_MAPPINGS = [
-        ["id", "id"], ["symbol", "symbol"], ["createdAt", "created"],
-        ["parentId", "parent_id"], ["parameterName", "name"],
-        ["parameterValue", "value"]
-    ];
+function RepositoryService(gatewayService, factoryService){
+
     let _environmentRepository;
     this.getEnvironmentRepository = () =>{
         if(_environmentRepository) return _environmentRepository;
-        let db = dataSourceService.getDatabaseConnection(DB_FILE);
+        const ENVIRONMENT_MAPPINGS = [
+            ["id", "id"], ["symbol", "symbol"], ["createdAt", "created"],
+            ["parentId", "parent_id"], ["parameterName", "name"],
+            ["parameterValue", "value"]
+        ];
         let environmentFactory = factoryService.getEnvironmentFactory();
-        _environmentRepository = new SqlRepositoryBuilder()
-            .setDbConnection(db)
-            .setDefinition(ENVIRONMENT_SQL)
-            .setFactory(environmentFactory)
-            .setMappings(ENVIRONMENT_MAPPINGS)
+        let environmentViewReader = gatewayService.getEnvironmentReader();
+        let sqlStatementFactory = factoryService.getSqlStatementFactory(ENVIRONMENT_MAPPINGS);
+        _environmentRepository = new RepositoryBuilder()
+            .setReader(environmentViewReader)
+            .setEntityFactory(environmentFactory)
+            .setGatewayRequestFactory(sqlStatementFactory)
             .build();
         return _environmentRepository;
     };
