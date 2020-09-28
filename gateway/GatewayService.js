b004330f89e463e3f249fa70f1b82aa3d303fcbd
yuryprokashev <yury.prokashev@edufun.me>
2020-09-28 19:29:31 +0300

    gateway service to use datasource config to obtain datasources

--- gateway/GatewayService.js
+++ gateway/GatewayService.js
@@ -1,20 +1,8 @@
 const SqlTableReader = require("./SqlTableReader");
 module.exports = GatewayService;
-function GatewayService(dataSourceService){
-    const DB_FILE = "C:/Users/yuryp/AppData/Roaming/MetaQuotes/Terminal/Common/Files/mt5.sqlite";
-    const ENVIRONMENT_SQL = "select p.id, e.id as parent_id, e.symbol, e.created, p.name, p.value from parameters as p left join environments as e on e.id = p.parent_id";
-    let _environmentViewReader;
-    this.getEnvironmentReader = ()=>{
-        if(!_environmentViewReader){
-            let db = dataSourceService.getDatabaseConnection(DB_FILE);
-            _environmentViewReader = new SqlTableReader(db, ENVIRONMENT_SQL);
-        }
-        return _environmentViewReader;
-    };
-    this.getVirtualOrderWriter = ()=>{
-
-    };
-    this.getParameterWriter = ()=>{
-
+function GatewayService(dsConfig, dataSourceService){
+    const _db = dataSourceService.getDatabaseConnection(dsConfig.db.file, dsConfig.db.verbose);
+    this.getSqlReader = definition =>{
+        return new SqlTableReader(_db, definition);
     };
 }
