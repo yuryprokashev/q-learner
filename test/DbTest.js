3703970f940a22b333beb8f2e8ae95bed03591d8
yuryprokashev <yury.prokashev@edufun.me>
2020-09-30 08:29:51 +0300

    tests for db transaction insert

--- test/DbTest.js
+++ test/DbTest.js
@@ -1,5 +1,21 @@
 module.exports = testEnvironment =>{
-    QUnit.module("better-sqlite3");
+    QUnit.module("better-sqlite3", {
+        before: ()=>{
+            testEnvironment.db.prepare(`
+                    create table tests
+                    (
+                        id     text not null
+                            constraint tests_pk
+                                primary key,
+                        symbol text not null,
+                        reward real not null
+                    )`
+            ).run();
+        },
+        after: ()=>{
+            testEnvironment.db.prepare("drop table if exists tests").run();
+        }
+    });
     let statement;
     QUnit.test("Prepare Statement", assert =>{
         assert.expect(0);
@@ -8,5 +24,21 @@ module.exports = testEnvironment =>{
     QUnit.test("Execute Statement", assert=>{
         assert.expect(0);
         statement.all(1577982720000.0, 1577982780000.0);
-    })
+    });
+    QUnit.test("Run Insert Many in Transaction", assert =>{
+        const columns = "id,symbol,reward";
+        const values = [[1,'MSFT', 0.002], [2, 'MSFT', 0.006]];
+        const statement = testEnvironment.db.prepare(`insert into tests (${columns}) values (?, ?, ?)`);
+        const insertTransaction = testEnvironment.db.transaction(vectors =>{
+            const result = [];
+            for(const vector of vectors) {
+                result.push(statement.run(vector));
+            }
+            return result;
+        });
+        const response = insertTransaction(values);
+        assert.strictEqual(response.length, 2);
+        assert.strictEqual(response[0].lastInsertRowid, 1);
+        assert.strictEqual(response[1].lastInsertRowid, 2);
+    });
 }
