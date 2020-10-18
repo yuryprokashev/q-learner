57069c4fae594dd72bf5d59c01f67d3821728e8c
yuryprokashev <yury.prokashev@edufun.me>
2020-10-18 11:03:57 +0300

    fix: environments returned in descending order, timebuckets in ascending. so when the environments are grouped into the timebuckets, they are not overlapping

--- apps/EnvironmentApp.js
+++ apps/EnvironmentApp.js
@@ -3,7 +3,7 @@ const EnvironmentFactory = require("../factory/EnvironmentFactory");
 module.exports = EnvironmentApp;
 function EnvironmentApp(io){
     const BASE_SQL = "select p.id, e.id as parent_id, e.symbol, e.created, p.name, p.value from parameters as p left join environments as e on e.id = p.parent_id";
-    const CREATED_SQL = "where created >= ? and created <= ?";
+    const CREATED_SQL = "where created >= ? and created <= ? order by created asc";
     const _envFactory = new EnvironmentFactory();
     this.getByCreatedDate = (start, end) =>{
         const db = io.getDatabase();
