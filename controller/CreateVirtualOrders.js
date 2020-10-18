8f88af945cf7f6ac9137cf81be4b410f6bf47356
yuryprokashev <yury.prokashev@edufun.me>
2020-10-18 11:12:25 +0300

    fix: qLearner instance must be used by the controller, not created by it

--- controller/CreateVirtualOrders.js
+++ controller/CreateVirtualOrders.js
@@ -1,18 +1,13 @@
 const CreateVirtualOrderRequestFactory = require("../factory/CreateVirtualOrderRequestFactory");
-const QLearnerFacade = require("../QLearner");
 module.exports = CreateVirtualOrders;
-function CreateVirtualOrders(env, userRequest){
-    /*
-    Создаем фасад системы
-     */
-    const qLearnerFacade = new QLearnerFacade(env);
+function CreateVirtualOrders(qLearner, userRequest){
     /*
     Просим ее создать виртуальные ордера
      */
     const createOrdersRequest = new CreateVirtualOrderRequestFactory().create(userRequest);
-    const orders = qLearnerFacade.createVirtualOrders(createOrdersRequest);
+    const orders = qLearner.createVirtualOrders(createOrdersRequest);
     /*
     Просим ее записать эти ордера в базу
      */
-    qLearnerFacade.saveVirtualOrders(orders);
+    qLearner.saveVirtualOrders(orders);
 }
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-const ComputeRewardUserController = require("./controller/ComputeRewardsUserController");
-const ctrl = new ComputeRewardUserController();
-ctrl.onUserRequest();
+const CreateVirtualOrdersUserRequest = require("./controller/CreateVirtualOrdersUserRequest");
+const CreateVirtualOrders = require("./controller/CreateVirtualOrders");
+const QLearnerFacade = require("./QLearner");
+
+const userRequest = new CreateVirtualOrdersUserRequest("2020-01-02", 0, 10, 1);
+const qLearner = new QLearnerFacade("prod");
+CreateVirtualOrders(qLearner, userRequest);
