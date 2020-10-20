3fcb5606099269f70f4ec808864f685fd037c6e2
yuryprokashev <yury.prokashev@edufun.me>
2020-10-20 20:54:32 +0300

    environmentApp can get the environment by id and can get the reference environment by the current environment and the delta T reference.

--- system/apps/EnvironmentApp.js
+++ system/apps/EnvironmentApp.js
@@ -4,6 +4,8 @@ module.exports = EnvironmentApp;
 function EnvironmentApp(io){
     const BASE_SQL = "select p.id, e.id as parent_id, e.symbol, e.created, p.name, p.value from parameters as p left join environments as e on e.id = p.parent_id";
     const CREATED_SQL = "where created >= ? and created <= ? order by created asc";
+    const REF_DT = 60*1000; // 60 секунд.
+    const REF_ENV_SQL = `${CREATED_SQL} limit 1`;
     const _envFactory = new EnvironmentFactory();
     this.getByCreatedDate = (start, end) =>{
         const db = io.getDatabase();
@@ -11,5 +13,22 @@ function EnvironmentApp(io){
         const statement = db.prepare(template);
         const records = statement.all(start, end);
         return _envFactory.fromRecords(records);
+    };
+    this.getReferenceEnvironment = (currentEnvironment, refDt) =>{
+        const _refDt = refDt ? refDt : REF_DT;
+        const db = io.getDatabase();
+        const template = `${BASE_SQL} ${REF_ENV_SQL}`;
+        const statement = db.prepare(template);
+        const start = currentEnvironment.getCreatedAt() - _refDt;
+        const end = currentEnvironment.getCreatedAt();
+        const records = statement.all(start, end);
+        return _envFactory.fromRecords(records)[0];
+    };
+    this.getById = environmentId =>{
+        const db = io.getDatabase();
+        const template = `${BASE_SQL} where p.parent_id = '${environmentId}'`;
+        const statement = db.prepare(template);
+        const records = statement.all();
+        return _envFactory.fromRecords(records)[0];
     };
 }
--- system/test/EnvironmentAppTest.js
+++ system/test/EnvironmentAppTest.js
@@ -12,4 +12,14 @@ module.exports = (io)=>{
         const environments = this.environmentApp.getByCreatedDate(start, end);
         assert.strictEqual(environments.length, 6);
     });
+    QUnit.test("Can get environment by id", assert =>{
+        const environment = this.environmentApp.getById("environment-_MSFT-1577982720000");
+        assert.strictEqual(environment.getId(), "environment-_MSFT-1577982720000");
+        assert.strictEqual(environment.getParameter("bid").getValue(), "158.310000000000002274");
+    });
+    QUnit.test("Can get reference environment by current Environment and reference delta T", assert =>{
+         const currentEnvironment = this.environmentApp.getById("environment-_MSFT-1577982780476");
+         const refEnvironment = this.environmentApp.getReferenceEnvironment(currentEnvironment, 60000);
+         assert.strictEqual(refEnvironment.getId(), "environment-_MSFT-1577982720779");
+    });
 };
