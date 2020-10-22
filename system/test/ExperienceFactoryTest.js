fa6d453f8947e4273c55b25556f71aadb57f0a39
yuryprokashev <yury.prokashev@edufun.me>
2020-10-22 06:11:34 +0300

    experience parameters test: split existing test to two and added more details about expectations from experience parameters

--- system/test/ExperienceFactoryTest.js
+++ system/test/ExperienceFactoryTest.js
@@ -8,21 +8,29 @@ module.exports = (io)=>{
         }
     });
     QUnit.test("Can create Experience from current Environment and Reference Environment", assert =>{
-        const currentEnvironment = this.environmentApp.getById("environment-_MSFT-1577982780476");
-        const refEnvironment = this.environmentApp.getById("environment-_MSFT-1577982720779");
-        const experience = this.experienceFactory.create(currentEnvironment, refEnvironment);
-        assert.strictEqual(experience.getId(), "environment-_MSFT-1577982780476-environment-_MSFT-1577982720779");
-
-        const currentMa10 = currentEnvironment.getParameter("ma10");
-        const refMa160 = refEnvironment.getParameter("ma160");
-        const delta = currentMa10.getValue() - refMa160.getValue();
-        const experienceParameterMa10Ma160 = experience.getParameter("current-ma10-ref-ma160");
-        assert.strictEqual(experienceParameterMa10Ma160.getValue(), delta);
-
-        const experienceParameterLength = currentEnvironment.getParameters().reduce((acc, param, index)=>{
+        this.currentEnvironment = this.environmentApp.getById("environment-_MSFT-1577982780476");
+        this.refEnvironment = this.environmentApp.getById("environment-_MSFT-1577982720779");
+        this.experience = this.experienceFactory.create(this.currentEnvironment, this.refEnvironment);
+        assert.strictEqual(this.experience.getId(), "environment-_MSFT-1577982780476-environment-_MSFT-1577982720779");
+    });
+    QUnit.test("The value of experience parameter is Current_Env_Parameter_i - Ref_Env_Parameter_j", assert =>{
+        const currentMa10 = this.currentEnvironment.getParameter("ma10");
+        const refMa160 = this.refEnvironment.getParameter("ma160");
+        const experienceParameterValue = currentMa10.getValue() - refMa160.getValue();
+        const experienceParameterMa10Ma160 = this.experience.getParameter("current-ma10-ref-ma160");
+        assert.strictEqual(experienceParameterMa10Ma160.getValue(), experienceParameterValue);
+    })
+    QUnit.test("The number of parameters in one experience is SUM(7..1) when Environment parameter count is 7", assert =>{
+        /*
+        Мы считаем разность current(i) - ref(j) только для тех j, которые больше или равны i.
+        Таким образом получается что если i = j = 7, то для i = 1 мы пройдем по j от 1 до 7 (всего 7).
+        Для i = 2  мы пройдем по j  от 2 до 7 (всего 6).
+        То есть кол-во параметров будет 7+6+5...+2+1 = 28.
+         */
+        const experienceParameterLength = this.currentEnvironment.getParameters().reduce((acc, param, index)=>{
             acc += (index+1);
             return acc;
         },0);
-        assert.strictEqual(experience.getParameters().length, experienceParameterLength);
-    });
+        assert.strictEqual(this.experience.getParameters().length, experienceParameterLength);
+    })
 }
