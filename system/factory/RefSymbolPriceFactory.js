b7a82a39f3419fef97106e0a148d6c06a4f5dc27
yuryprokashev <yury.prokashev@edufun.me>
2020-11-01 13:45:07 +0300

    cleaner ref symbol price formula

--- system/factory/RefSymbolPriceFactory.js
+++ system/factory/RefSymbolPriceFactory.js
@@ -2,5 +2,7 @@
 Референсная цена акции - это среднее курса покупки и курса продажи.
  */
 module.exports = (refEnvironment)=>{
-    return (refEnvironment.getParameter("bid").getValue() + refEnvironment.getParameter("ask").getValue())/2;
+    const bid = refEnvironment.getParameter("bid").getValue();
+    const ask = refEnvironment.getParameter("ask").getValue();
+    return (bid + ask)/2;
 }
