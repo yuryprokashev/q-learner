76b98e4623c3c7b1a9b884e0ef9a33ce14d391b7
yuryprokashev <yury.prokashev@edufun.me>
2020-12-17 09:07:21 +0300

    fix: experience code formula

--- system/factory/ExperienceParameterCodeFactory.js
+++ system/factory/ExperienceParameterCodeFactory.js
@@ -2,5 +2,5 @@
 Усиливаем сигнал в 10000 раз и отрезаем десятичные чтобы получить целое.
  */
 module.exports = parameterValue =>{
-    return Math.trunc(parameterValue * 10000)
+    return Math.trunc(parameterValue * 1000)
 }
